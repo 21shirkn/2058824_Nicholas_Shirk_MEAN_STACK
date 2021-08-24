8702947de11b18a052e7fc1870c6610117c4afde
Nick <21shirkn@gmail.com>
2021-08-24 16:30:42 -0700

    phase 3 project 1

---
+++ Phase 3/dataLog/datalog.js
@@ -0,0 +1,48 @@
+let r1 = require("readline-sync");	
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+let user = {
+    Fname:r1.question("enter first name"),
+    Lname:r1.question("Enter Last name"),
+    gender: r1.question("enter gender"),
+    email: r1.question("enter email"),
+    time:dateTime
+    
+}
+debugger;
+
+let fs = require("fs");
+const path = './dataStorage.JSON';
+debugger
+if(fs.existsSync('dataStorage.JSON')){
+    fs.readFile("dataStorage.JSON", (err, data)=>{
+        if(!err){
+            var inmsg = []
+            var inmsgJSON = data.toString();
+            inmsg= JSON.parse(inmsgJSON);
+            console.log(inmsg);
+            inmsg.push(user);
+            inmsgJSON = JSON.stringify(inmsg);
+            fs.writeFile("dataStorage.JSON", inmsgJSON, (err)=> {
+                if(!err){
+                    console.log("Data stored in file successfully");
+                    debugger;
+                }
+            })
+        }
+    });
+}else{
+    let dataStorage = []
+
+    dataStorage.push(user);
+    let inmsgJSON = JSON.stringify(dataStorage);
+        fs.writeFile("dataStorage.JSON", inmsgJSON, (err)=> {
+            if(!err){
+                console.log("Data stored in file successfully");
+                debugger;
+            }
+        })
+
+}
